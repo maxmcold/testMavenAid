0a01d909d167b1aa37f46fda44eff7ff1934c97e
Massimo del Vecchio <massimo.delvecchio@channelvas.com>
2018-04-11 19:40:44 +0300

    Added test object

---
+++ src/main/java/MySingleton.java
@@ -0,0 +1,12 @@
+public class MySingleton {
+
+    private static MySingleton ourInstance = new MySingleton();
+
+    public static MySingleton getInstance() {
+        return ourInstance;
+    }
+
+    private MySingleton() {
+
+    }
+}
